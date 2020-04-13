2599403110ea4de5ee7d90cef2b0edaf817426d3
“gusstavotelles” <gustavinho-telles2011@hotmail.com>
2020-04-13 14:06:16 -0300

    correção porta para heroku

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 
 const app = express();
-const PORT = 3333;
+// const PORT = 3333;
 
 app.use(express.urlencoded({ extended: false }));
 
@@ -10,6 +10,6 @@ app.use(cors());
 
 app.use(require('./routes'));
 
-app.listen(PORT, () => {
+app.listen(PORT || 3333, () => {
     console.log(`Server started at: http://localhost:${PORT}`);
 });
