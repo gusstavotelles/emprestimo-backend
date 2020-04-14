c05929229d73805d38c2bd13c4592ff22659447a
“gusstavotelles” <gustavinho-telles2011@hotmail.com>
2020-04-14 03:30:24 -0300

    correção port env

--- index.js
+++ index.js
@@ -10,6 +10,6 @@ app.use(cors());
 
 app.use(require('./routes'));
 
-app.listen(PORT || 3333, () => {
+app.listen(process.env.PORT || 3333, () => {
     //console.log(`Server started at: http://localhost:${PORT}`);
 });
